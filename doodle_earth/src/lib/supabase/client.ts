0574804bbe50466595e4a189e18cf2d2ca70f45c
Souren33 <prakashsouren@gmail.com>
2025-10-12 13:53:27 -0400

    crud ops in progress

--- doodle_earth/src/lib/supabase/client.ts
+++ doodle_earth/src/lib/supabase/client.ts
@@ -7,8 +7,49 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 const user = "TestUser" // Make this dynamic
 
+
+
+
 const { data, error } = await supabase
   .from('User Profiles')
   .select()
   .eq("name", {user})
 
+// Create
+// Create commissions, submission, add or remove friends
+
+// Creating commission
+const commission = 'Mordor' // make dynamic
+const id = crypto.randomUUID() // generate unique id for each commission
+
+const { data, error } = await supabase
+  .from('Commissions')
+  .insert({ id , commission })
+
+// Creating submission
+
+const submission = 'Gandalf' // make dynamic
+const commission_id = crypto.randomUUID() // make dynamic
+const { data, error } = await supabase
+
+
+
+// READ
+// View, existing commissions, prior commissions/submission
+
+// Make nested query for this s
+const { data, error } = await supabase
+  .from('Commissions')
+  .select('image_url')
+  .eq("commission_id", {commission_id})
+
+
+
+
+// Update
+// udpdate their own posts, information, account, bio, profile picture
+
+
+
+// DELETE
+// Delete their own posts, information, account 
