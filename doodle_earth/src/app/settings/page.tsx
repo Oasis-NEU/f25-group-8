95e8c88e99eebdda8512332dfeaa673f19aecf3c
lujan002 <20JansenLu@gmail.com>
2025-10-26 13:31:16 -0400

    added supabase test to settings page

--- doodle_earth/src/app/settings/page.tsx
+++ doodle_earth/src/app/settings/page.tsx
@@ -1,13 +1,32 @@
 // Settings menu page
 "use client"
 
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 
 const SettingsPage = () => {
-  return (
-    <div>Placeholder Settings Page</div>
+  const [instruments, setInstruments] = useState([]);
 
+  useEffect(() => {
+    getInstruments();
+  }, []);
+
+  async function getInstruments() {
+    const { data } = await supabase.from("instruments").select();
+    setInstruments(data);
+  }
+
+  return (
+    <div>
+      <div>Placeholder Settings Page</div>
+      <ul>
+        {instruments.map((instrument) => (
+          <li key={instrument.name}>{instrument.name}</li>
+        ))}
+      </ul>
+    </div>
   )}
   
 export default SettingsPage;
