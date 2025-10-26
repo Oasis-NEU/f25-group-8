5c0ed80a1011ab5824efd840dbf3545debde062b
lujan002 <20JansenLu@gmail.com>
2025-10-26 13:47:59 -0400

    renamed VITE to NEXT in keys

--- doodle_earth/src/app/settings/page.tsx
+++ doodle_earth/src/app/settings/page.tsx
@@ -4,8 +4,8 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL!
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
